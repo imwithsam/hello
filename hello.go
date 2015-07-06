39265afc77cd34b08dd728793fc6a4e3663d95a8
Samson Brock <SamsonLB@gmail.com>
2015-07-05 19:46:21 -0600

    Add Reverse function

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/imwithsam/stringutil"
+)
 
 func main() {
-    fmt.Printf("Hello, world.\n")
+	fmt.Printf(stringutil.Reverse("Hello, world!"))
 }
